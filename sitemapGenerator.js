696ae5ad0c438625d87c6ce229b2269b28227b7b
Komei Otake <komei1220@gmail.com>
2014-08-26 20:17:58 +0900

    implement: http server

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -1,25 +1,23 @@
 var fs = require('fs');
-var class = require('Class');
-
-
-var class1 = new Class( "" );
 
 fs.readdir( './html', function( i_error, i_files ) {
 
-    if( i_error ) {
-    	throw i_error;
-    }
+	if( i_error ) {
+		throw i_error;
+	}
 
-    var fileList = [];
+	var fileList = [];
 
-    i_files.filter( function( i_file ) {
-        return fs.statSync( i_file ).isFile() && /.*\.csv$/.test( i_file ); //絞り込み
-    }).forEach( function( i_file ) {
+	i_files.filter( function( i_file ) {
 
-        fileList.push( i_file );
+		return fs.statSync( i_file ).isFile().test( i_file ); //絞り込み
+		console.log(fs.statSync( i_file ).isFile().test( i_file ));
+	}).forEach( function( i_file ) {
 
-    });
+		fileList.push( i_file );
 
-    console.log(fileList);
+	});
+
+	console.log(fileList);
 
 });
