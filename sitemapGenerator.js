632e061ecf87f65c4e29603232a452e5db07e7e7
Komei Otake <komei1220@gmail.com>
2014-08-27 19:56:32 +0900

    refactor arg

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -2,35 +2,35 @@ var fs   = require('fs');
 var path = require('path');
 
 // ディレクトリかチェック
-var isDir = function(filepath) {
-	if (/\/$/.test(filepath)) {
-		filepath = filepath.substr(0, filepath.lastIndexOf('/') - 1);
+var isDir = function( i_path ) {
+	if ( /\/$/.test( i_path ) ) {
+		i_path = i_path.substr( 0, i_path.lastIndexOf('/') - 1 );
 	}
-	return (path.existsSync(filepath) && fs.statSync(filepath).isDirectory());
+
+	return ( path.existsSync( i_path ) && fs.statSync( i_path ).isDirectory() );
 };
 
-
-var walk = function(p, callback) {
-	fs.readdir(p, function(err, files){
-	if (err) throw err;
-	var pending = files.length;
-	files.map(function(file){
-		// リスト取得
-		return path.join(p, file);
-	})
-	.filter(function(file){
-		if (fs.statSync(file).isDirectory()) {
-			walk(file, function(err, res){
-				if (err) throw err;
-			});
-		}
-		else {
-		console.log(file);
-		}
-	})
+var walk = function( i_path, callback ) {
+	fs.readdir( i_path, function( err, files ){
+		if ( err ) throw err;
+		var pending = files.length;
+		files.map( function( file ){
+			// リスト取得
+			return path.join( i_path, file );
+		})
+		.filter(function( file ){
+			if ( fs.statSync(file).isDirectory() ) {
+				walk( file, function( err, res ){
+					if ( err ) throw err;
+				});
+			}
+			else {
+				console.log( file );
+			}
+		})
 	});
 };
 
-walk('./html', function(err, results){
-	if (err) throw err;
+walk("./html", function( err, results ){
+	if ( err ) throw err;
 });
