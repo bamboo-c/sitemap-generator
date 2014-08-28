5830d0d67b686557f6d0c71f6d61f7ac9dacaa3e
Komei Otake <komei1220@gmail.com>
2014-08-29 00:41:33 +0900

    output of the JSON

--- sitemapGenerator.js
+++ sitemapGenerator.js
@@ -1,36 +1,35 @@
-var fs   = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
+var dir = process.argv[2] || '.';
 
-// ディレクトリかチェック
-var isDir = function( i_path ) {
-	if ( /\/$/.test( i_path ) ) {
-		i_path = i_path.substr( 0, i_path.lastIndexOf('/') - 1 );
-	}
+var walk = function(p, callback){
+	var results = [];
 
-	return ( path.existsSync( i_path ) && fs.statSync( i_path ).isDirectory() );
-};
+	fs.readdir(p, function (err, files) {
+		if (err) throw err;
 
-var walk = function( i_path, callback ) {
-	fs.readdir( i_path, function( err, files ){
-		if ( err ) throw err;
 		var pending = files.length;
-		files.map( function( file ){
-			// リスト取得
-			return path.join( i_path, file );
-		})
-		.filter(function( file ){
-			if ( fs.statSync(file).isDirectory() ) {
-				walk( file, function( err, res ){
-					if ( err ) throw err;
-				});
-			}
-			else {
-				console.log( file );
-			}
-		})
-	});
-};
+		if (!pending) return callback(null, results); //全てのファイル取得が終わったらコールバックを呼び出す
 
-walk("./html", function( err, results ){
-	if ( err ) throw err;
+		files.map(function (file) { //リスト取得
+			return path.join(p, file);
+		}).filter(function (file) {
+			if(fs.statSync(file).isDirectory()) walk(file, function(err, res) { //ディレクトリだったら再帰
+				results.push({name:path.basename(file), children:res}); //子ディレクトリをchildrenインデックス配下に保存
+				if (!--pending) callback(null, results);
+			 });
+			return fs.statSync(file).isFile();
+		}).forEach(function (file) { //ファイル名を保存
+			var stat = fs.statSync(file);
+			results.push({file:path.basename(file), size:stat.size});
+			if (!--pending) callback(null, results);
+		});
+
+	});
+}
+
+walk(dir, function(err, results) {
+	if (err) throw err;
+	var data = {name:'root', children:results};
+	console.log(JSON.stringify(data)); //一覧出力
 });
